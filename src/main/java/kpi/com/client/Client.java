ffa57f71b40323fc5035027d112a5c4b8184e246
gracethomp <gracetomlinson26@gmail.com>
2023-12-19 21:56:18 +0200

    reformat client: scanner for word input

--- src/main/java/kpi/com/client/Client.java
+++ src/main/java/kpi/com/client/Client.java
@@ -4,64 +4,34 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
     private final String HOST;
     private final int PORT;
 
-    public Client(String host, int port){
+    public Client(String host, int port) {
         this.HOST = host;
         this.PORT = port;
     }
 
-    public void sendMatrix(int n, int m) {
-        int[][] matrix = generateMatrix(n, m);
-        try(Socket socket = new Socket(HOST, PORT)) {
+    public void connect() {
+        try (Socket socket = new Socket(HOST, PORT)) {
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
             System.out.println(dataInputStream.readUTF());
-            writeMatrix(dataOutputStream, matrix);
-
-            System.out.println(dataInputStream.readUTF());
-            dataOutputStream.writeUTF("start");
-            System.out.println(dataInputStream.readUTF());
-            while (true) {
-                dataOutputStream.writeUTF("get");
-                String response = dataInputStream.readUTF();
-                System.out.println(response);
-                if (!response.equalsIgnoreCase("In progress"))
-                    break;
-                Thread.sleep(1000);
-            }
-
+            Scanner sc = new Scanner(System.in);
+            String word = sc.nextLine();
+            dataOutputStream.writeUTF(word);
             dataInputStream.close();
             dataOutputStream.close();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static int[][] generateMatrix(int n, int m){
-        Random random = new Random();
-        int[][] matrix = new int[n][m];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++){
-                matrix[i][j] = random.nextInt(10001);
-            }
-        }
-        return matrix;
-    }
-
-    private static void writeMatrix(DataOutputStream out, int[][] matrix) throws IOException {
-        out.writeInt(matrix.length);
-        out.writeInt(matrix[0].length);
-        for (int[] ints : matrix) {
-            for (int anInt : ints) {
-                out.writeInt(anInt);
-            }
-        }
+    private void sendWord(DataOutputStream outputStream, String word) throws IOException {
+        outputStream.writeUTF(word);
     }
 }
 
