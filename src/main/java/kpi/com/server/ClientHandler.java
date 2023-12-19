f8111015bbd8aa3ca09c754e7c2ec4ef8e1d1c35
gracethomp <gracetomlinson26@gmail.com>
2023-12-19 22:55:12 +0200

    infinite search for client

--- src/main/java/kpi/com/server/ClientHandler.java
+++ src/main/java/kpi/com/server/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
+import java.util.Objects;
 
 public class ClientHandler extends Thread {
     private final Socket socket;
@@ -22,37 +23,19 @@ public class ClientHandler extends Thread {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
-            outputStream.writeUTF("Please send word you want to find");
-            String word = inputStream.readUTF();
-            System.out.println(word);
-            invertedIndex.addDocument(1, "Java is a programming language");
-            invertedIndex.addDocument(2, "Python is also a programming language");
-            invertedIndex.addDocument(3, "Java and Python are popular languages");
-            List<Integer> result = invertedIndex.search(word);
-            outputStream.writeUTF(result.toString());
-//            int[][] matrix = readMatrix(inputStream);
-//
-//            outputStream.writeUTF("Data is received. Write 'start'");
-//            inputStream.readUTF();
-//            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
-//                try {
-//                    return findSum(matrix);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            });
-//
-//            outputStream.writeUTF("Calculation is started, please wait...");
-//            //int sum = findSum(matrix);
-//            while (true) {
-//                inputStream.readUTF();
-//                if(future.isDone()){
-//                    outputStream.writeUTF("Calculation was ended. The result is " + future.get());
-//                    break;
-//                } else {
-//                    outputStream.writeUTF("In progress");
-//                }
-//            }
+            boolean continueConfirmation = true;
+
+            while (continueConfirmation) {
+                outputStream.writeUTF("Please send word you want to find");
+                String word = inputStream.readUTF();
+                System.out.println(word);
+                invertedIndex.addDocument(1, "Java is a programming language");
+                invertedIndex.addDocument(2, "Python is also a programming language");
+                invertedIndex.addDocument(3, "Java and Python are popular languages");
+                List<Integer> result = invertedIndex.search(word);
+                outputStream.writeUTF(result.toString() + ". Did you want to continue? (y/n)");
+                continueConfirmation = Objects.equals(inputStream.readUTF(), "y");
+            }
 
             outputStream.close();
             inputStream.close();
