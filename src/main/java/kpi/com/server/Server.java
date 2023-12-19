f1a88ee7dcdb7b4aba6a27dc2f65e7c41400142a
gracethomp <gracetomlinson26@gmail.com>
2023-12-19 21:53:09 +0200

    add thread pool

--- src/main/java/kpi/com/server/Server.java
+++ src/main/java/kpi/com/server/Server.java
@@ -2,9 +2,12 @@ package kpi.com.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;
 
     public static void main(String[] args) {
         Server server = new Server();
@@ -14,6 +17,7 @@ public class Server {
     public void startServer() {
         try {
             serverSocket = new ServerSocket(6666);
+            threadPool = Executors.newFixedThreadPool(5);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -22,7 +26,8 @@ public class Server {
             try {
                 ClientHandler clientHandler = new ClientHandler(serverSocket.accept());
                 System.out.println("A new client is connected...");
-                clientHandler.start();
+//                clientHandler.start();
+                threadPool.execute(clientHandler);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
